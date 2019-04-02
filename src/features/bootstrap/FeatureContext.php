f30e89bc4b0b35ecf698381fc9b7ce22b4e24a1d
Nikola Paunovic <nikola@applycolo.rs>
2019-04-02 23:05:43 +0200

    Creation of lead by steps

--- src/features/bootstrap/FeatureContext.php
+++ src/features/bootstrap/FeatureContext.php
@@ -17,6 +17,13 @@ class FeatureContext implements Context
     /** @var Lead */
     private $lead;
 
+    private $firstName;
+
+    private $lastName;
+
+    private $email;
+
+    private $phone;
 
     /**
      * @When user submits firstname :arg1, lastname :arg2, email :arg3, phone number :arg4
@@ -38,5 +45,67 @@ class FeatureContext implements Context
     public function leadShouldBeCreatedWithReferral($arg1)
     {
         Assert::assertEquals($arg1, $this->lead->referral());
+    }
+
+    /**
+     * @Given that lead is on online vacancy application form
+     */
+    public function thatLeadIsOnOnlineVacancyApplicationForm()
+    {
+        $this->lead = null;
+    }
+
+    /**
+     * @Given enters :arg1 in firstname
+     */
+    public function entersInFirstname($arg1)
+    {
+        $this->firstName = $arg1;
+    }
+
+    /**
+     * @Given enters :arg1 in lastname
+     */
+    public function entersInLastname($arg1)
+    {
+        $this->lastName = $arg1;
+    }
+
+    /**
+     * @Given enters :arg1 in email
+     */
+    public function entersInEmail($arg1)
+    {
+        $this->email = $arg1;
+    }
+
+    /**
+     * @Given enters :arg1 in phone number
+     */
+    public function entersInPhoneNumber($arg1)
+    {
+        $this->phone = $arg1;
+    }
+
+    /**
+     * @Given submits data
+     */
+    public function submitsData()
+    {
+        $this->lead = Lead::appliesByHimself(
+            Uuid::uuid4()->toString(),
+            new FirstName($this->firstName),
+            new LastName($this->lastName),
+            new Email($this->email),
+            new PhoneNumber($this->phone)
+        );
+    }
+
+    /**
+     * @Then lead should be created with applied-by-himself referral
+     */
+    public function leadShouldBeCreatedWithAppliedByHimselfReferral()
+    {
+        Assert::assertEquals('applied-by-himself', $this->lead->referral());
     }
 }
--- src/features/funnel.feature
+++ src/features/funnel.feature
@@ -9,7 +9,15 @@ Feature: Lead creation - app should be able to create Lead
   - Phone number is filled and valid
   - Email is filled and valid
 
-  Scenario:
+  Scenario: User submits data into application form on website with valid data
     When user submits firstname "first name", lastname "last name", email "test@test.com", phone number "+490160123123123"
     Then Lead should be created with referral "applied-by-himself"
 
+  Scenario: User submits data into application form on website with valid data in steps
+    Given that lead is on online vacancy application form
+    And enters "first name" in firstname
+    And enters "last name" in lastname
+    And enters "test@test.com" in email
+    And enters "+490160123123123" in phone number
+    And submits data
+    Then lead should be created with applied-by-himself referral
